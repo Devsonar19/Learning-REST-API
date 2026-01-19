a5b71d258bb645adffa5e50996c7bfb4ab7342b7
https://github.com/Devsonar19/My-Notes.git <devsonar19@outlook.com>
2026-01-19 23:51:37 +0530

    Creating and Storing in Array

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 
 app = FastAPI()
@@ -13,6 +14,19 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
+my_posts = [
+    {
+        "title":"example post 1",
+        "content":"Exampleeee",
+        "id":1 
+    },
+    {
+        "title":"example post 2",
+        "content":"ExampleeeeYYYY",
+        "id":2
+    }
+]
+
 @app.get("/")   #Decorator, without it, its simple python
 def root():
     return {"message": "Hello World From Ubuntu"}
@@ -20,10 +34,11 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "this is your post"}
+    return {"Using Array": my_posts}
 
-@app.post("/createpost")
+@app.post("/posts")
 def create_posts(newPost: Post):
-    print(newPost.rating)
-    print(newPost.dict)
-    return {"Data": newPost}
+    post_dict = newPost.dict()
+    post_dict['id'] = randrange(0, 100000000)
+    my_posts.append(post_dict)
+    return {"Data": post_dict}
