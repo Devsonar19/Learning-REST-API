72a7f28ad37fd70f062632d8b88054220d7695bf
https://github.com/Devsonar19/My-Notes.git <devsonar19@outlook.com>
2026-01-28 23:47:25 +0530

    used tags for sectioning in-built docs

--- app/routers/post.py
+++ app/routers/post.py
@@ -5,7 +5,8 @@ from ..database import get_db
 from typing import List
 
 router = APIRouter(
-    prefix="/posts" + id
+    prefix="/posts",
+    tags=['Posts']
 )
 
 
--- app/routers/user.py
+++ app/routers/user.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import Session
 from ..database import get_db
 
 router = APIRouter(
-    prefix="/users" + id
+    prefix="/users",
+    tags=['Users']
 )
 
 
