133ca297e2c8ec4fff735f5e217193bbc080847b
https://github.com/Devsonar19/My-Notes.git <devsonar19@outlook.com>
2026-01-28 23:12:14 +0530

    implemented getting user by id

--- app/main.py
+++ app/main.py
@@ -153,3 +153,12 @@ def create_user(newUser: schemas.UserCreate, db : Session = Depends(get_db)):
     db.refresh(newUser)
 
     return newUser
+
+@app.get('/users/{id}', response_model=schemas.UserOp)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with {id} not found")
+    
+    return user
