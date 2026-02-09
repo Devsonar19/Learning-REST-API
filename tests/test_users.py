78d037e45a92be0d57f1c20c6b8f151fb93f17c5
https://github.com/Devsonar19/My-Notes.git <devsonar19@outlook.com>
2026-02-09 23:20:47 +0530

    separated database to a diff file, better structure

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,47 +1,5 @@
-from fastapi.testclient import TestClient
 from app import schemas
-from app.database import get_db, Base
-from app.main import app
-from app import schemas
-from app.config import settings
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-import os
-import pytest
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_engine(DATABASE_URL)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-
-@pytest.fixture
-def session():
-    # we can run after test finishes
-    Base.metadata.dropall(bind=engine)
-    # we can run code before we run our test
-    Base.metadata.createall(bind=engine)
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@pytest.fixture
-def client(session):
-    def override_get_db():
-        try:
-            yield session
-        finally:
-            session.close()
-    app.dependency_overrides[get_db] = override_get_db
-    yield TestClient(app)
-    
-
+from .database import client, session
 
 
 def test_root(client):
