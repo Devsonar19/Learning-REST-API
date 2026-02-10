1197ae8d257a70ad5072e1060de681de356f1b26
https://github.com/Devsonar19/My-Notes.git <devsonar19@outlook.com>
2026-02-10 23:36:55 +0530

    incorrect login test, conftest file added for better management

---
+++ tests/conftest.py
@@ -0,0 +1,56 @@
+from app.config import settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import os
+import pytest
+from app.database import get_db, Base
+from app.main import app
+from fastapi.testclient import TestClient
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture
+def test_user(client):
+    user_data = {"email":"hello@gmail.com", "password":"123456"}
+    res = client.post("/users/",json=user_data)
+
+    assert res.status_code == 201
+    print(res.json())
+    new_user = res.json()
+    new_user = ['password'] = user_data['password']
+    return new_user
+
+
+
+@pytest.fixture()
+def session():
+    # we can run after test finishes
+    #running fixtures
+    Base.metadata.dropall(bind=engine)
+    # we can run code before we run our test
+    Base.metadata.createall(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture()
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+    
+
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,20 +1,8 @@
 from app import schemas
-from .database import client, session
-import pytest
 from jose import jwt
 from app.config import settings
+import pytest
 
-
-@pytest.fixture
-def test_user(client):
-    user_data = {"email":"hello@gmail.com", "password":"123456"}
-    res = client.post("/users/",json=user_data)
-
-    assert res.status_code == 201
-    print(res.json())
-    new_user = res.json()
-    new_user = ['password'] = user_data['password']
-    return new_user
 
 
 
@@ -44,3 +32,22 @@ def test_login_user(client, test_user):
     assert id ==  test_user['id']
     assert login_res.token_type == "bearer"
     assert res.status_code == 200
+
+@pytest.mark.parametrize("email, password, status_code",[
+    ('wrongemail@mail.com', 'RightPassword', 403),
+    ('rightemail@mail.com', 'WrongPassword', 403),
+    ('wrongemail@mail.com', 'WrongPassword', 403),
+    (None, 'RightPassword', 422),
+    ('rightemail@mail.com', None, 422)
+])    
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post(
+        "/login",
+        data={
+            "username":email,
+            "password":password,
+        }
+    )
+
+    assert res.status_code == status_code
+    assert res.json().get('detail') == 'Invalid Credentials'
