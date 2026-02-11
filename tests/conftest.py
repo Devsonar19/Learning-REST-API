a28e4e4f660c08e0f454f7e36300f9db1ab34d42
https://github.com/Devsonar19/My-Notes.git <devsonar19@outlook.com>
2026-02-11 21:46:26 +0530

    testing unauthorized user(all & individual

--- tests/conftest.py
+++ tests/conftest.py
@@ -96,3 +96,14 @@ def test_posts(test_user, session):
     session.commit()
     res = session.query(models.Post).all()
     return res
+
+def test_unauthorized_user_get_all_posts(client, test_posts):
+    res = client.get("/posts/")
+
+    assert res.status_code == 401
+
+
+def test_unauthorized_user_get_one_post(client, test_posts):
+    res = client.get(f"/posts/{test_posts[0].id}")
+
+    assert res.status_code == 401
